6f10addf142304ba607f9248053622c8e6c5571b
Yurina! <lihd1003@hotmail.com>
2019-07-27 21:07:34 -0400

    july28

--- get_availability.py
+++ get_availability.py
@@ -8,9 +8,13 @@ def process_info(table, fw):
             left = row[4].split("\n")[-4].replace("\n", "")
             fw.write(roomtype+"|"+price+"|"+left+"\n")
             
-def get_availabilities_from_listings(filename, date_in, driver):
+def get_availabilities_from_listings(filename, date_in, date_out, driver):
     """ Open filename, read the formated links, 
        and write the availablity of each listing to txt files"""
+    
+    year_in, month_in, day_in = date_in.split("-")
+    year_out, month_out, day_out = date_out.split("-")
+    
     f = open(filename, "r")
     links = [l[:-1] for l in f]
 
@@ -18,7 +22,7 @@ def get_availabilities_from_listings(filename, date_in, driver):
     index = 0
 
     fw = open("./avalibility/"+date_in+"_"+str(files)+".txt", "w")
-    print("---write to a"+date_in+str(files)+".txt---")
+    print("---write to: "+date_in+"_"+str(files)+".txt---")
 
     for address in links:
         index += 1
@@ -27,11 +31,21 @@ def get_availabilities_from_listings(filename, date_in, driver):
             fw.close()
             files += 1
             fw = open("./avalibility/"+date_in+"_"+str(files)+".txt", "w")
-            print("---write to a"+date_in+str(files)+".txt---")
+            print("---write to: "+date_in+"_"+str(files)+".txt---")
 
         fw.write("------" + address + "\n")
         try:
-            driver.get("https://www.booking.com/hotel/"+address+".en-gb.html?checkout_year=2019&checkout_month=07&checkout_monthday=28&checkin_year=2019&checkin_month=07&checkin_monthday=27&selected_currency=USD&changed_currency=1&top_currency=1&lang=en-gb&group_adults=1&no_rooms=1")
+            driver.get("https://www.booking.com/hotel/"+address+".en-gb.html?"+\
+                       "checkout_year=" +year_out+\
+                       "&checkout_month="+month_out+\
+                       "&checkout_monthday="+day_out+\
+                       "&checkin_year="+year_in+\
+                       "&checkin_month="+month_in+\
+                       "&checkin_monthday="+day_in+\
+                       "&selected_currency=USD"+\
+                       "&changed_currency=1"+\
+                       "&top_currency=1&lang=en-gb"+\
+                       "&group_adults=1&no_rooms=1")
             table = driver.find_element_by_css_selector(".hprt-table.hprt-table-long-language ")
             tbody = table.find_element_by_css_selector("tbody")
             trs = tbody.find_elements_by_css_selector("tr")
